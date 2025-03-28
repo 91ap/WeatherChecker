0791b2315d7f3a152191e320aedb210acef1b3bd
Andy Patterson <andyp.dev@proton.me>
2025-03-28 09:02:31 -0500

    [TASK] Removed api key.

---
+++ Model/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  WeatherChecker
+//
+//  Created by apat on 3/13/25.
+//
+
+import Foundation
---
+++ Model/WeatherModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherModel.swift
+//  WeatherChecker
+//
+//  Created by apat on 3/13/25.
+//
+
+import Foundation
